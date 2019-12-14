4080096d9f90fd389d797c77e5c8a18853ce5821
Christopher Pietsch <cpietsch@gmail.com>
2019-12-14 15:13:45 +0100

    making things more modular

--- src/App.vue
+++ src/App.vue
@@ -2,72 +2,51 @@
   <div id="app" ref="app">
     <Item>
       <template v-slot:figure="props">
-        <Slide2 :ratio="props.ratio" />
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/global-stocktake-1.svg" />
       </template>
     </Item>
     <Item>
       <template v-slot:figure="props">
-        <Slide3 :ratio="props.ratio" />
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/global-stocktake-2.svg" />
       </template>
     </Item>
     <Item>
       <template v-slot:figure="props">
-        <Slide2 :ratio="props.ratio" />
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/land-transitions.svg" />
       </template>
     </Item>
     <Item>
       <template v-slot:figure="props">
-        <Slide1 :ratio="props.ratio" />
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/extreme-events.svg" />
       </template>
     </Item>
-    <!-- <Item :scrollY="scrollY" :screenHeight="screenHeight">
-      <template slot="figure" scope="props">
-        <Slide2 :ratio="props.ratio" />
+    <Item>
+      <template v-slot:figure="props">
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/primary-energy.svg" />
       </template>
     </Item>
-    <Item :scrollY="scrollY" :screenHeight="screenHeight">
-      <template slot="figure" scope="props">
-        <Slide1 :ratio="props.ratio" />
+    <Item>
+      <template v-slot:figure="props">
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/sector-transition-1.svg" />
       </template>
-    </Item>-->
-    <!-- <Item>
-      <h1>headline blalallalal</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
-    </Item>-->
+    </Item>
+    <Item>
+      <template v-slot:figure="props">
+        <AnimatedSvg :ratio="props.ratio" svg="final-svg-portal/sector-transitions2.svg" />
+      </template>
+    </Item>
   </div>
 </template>
 
 <script>
 import Item from "./components/Item.vue";
-import Slide1 from "./components/Slide1.vue";
-import Slide2 from "./components/Slide2.vue";
-import Slide3 from "./components/Slide3.vue";
+import AnimatedSvg from "./components/AnimatedSvg.vue";
+
 let ticking = false;
 
 export default {
   name: "app",
-  components: { Item, Slide1, Slide2, Slide3 },
+  components: { Item, AnimatedSvg },
   methods: {
     start: function(e) {
       // console.log("start", e);
---
+++ src/components/AnimatedSvg.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="slide" ref="svg"></div>
+</template>
+
+<script>
+import vivus from "vivus";
+
+export default {
+  watch: {
+    ratio: function(ratio) {
+      this.vivus.setFrameProgress(ratio);
+      this.vivus.stop();
+    }
+  },
+  props: ["ratio", "svg"],
+  mounted: function() {
+    console.log(this.svg);
+    this.vivus = new vivus(this.$refs.svg, {
+      type: "oneByOne",
+      start: "manual",
+      file: this.svg
+    });
+    this.vivus.stop();
+  }
+};
+</script>
+
+<style scoped>
+.slide {
+  width: 934px;
+  height: 432px;
+}
+svg {
+  position: absolute;
+}
+</style>
